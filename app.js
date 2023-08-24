60fca7f217a342db40b5ec3df87eec41b3fb0b95
Manuel Henriquez <manuelhm1993@gmail.com>
2023-08-24 00:07:54 -0400

    Uso del objeto exports para exportar múltiples elementos y recibir con require el objeto

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 // Cuando se trabaja con nodejs no es necesario colocar la extensión .js equivalente a import frutas from "./modulos/frutas.js";
-const frutas = require('./modulos/frutas'); //require('./modulos/frutas.js'); // Opcional
+const moduloFrutas = require('./modulos/frutas'); //require('./modulos/frutas.js'); // Opcional 
 
-frutas.forEach((fruta) => {
+// Al usar require y se exportan varios elementos, se crea un objeto que contiene todo lo exportado
+console.log(moduloFrutas);
+
+moduloFrutas.frutas.forEach((fruta) => {
     console.count(fruta);
 });
+
+console.log(moduloFrutas.dinero);
--- modulos/frutas.js
+++ modulos/frutas.js
@@ -1,4 +1,12 @@
 const frutas = ['Plátano', 'Manzana', 'Plátano', 'Pera'];
+const dinero = 1000;
 
 // Usar el objeto module con la propiedad exports (equivalente a export default funcion;)
-module.exports = frutas;
+// module.exports = frutas;
+
+// De esta manera se exportan varios elementos, similar al export nombrado de ES6
+module.exports = {
+    frutas: frutas,
+    dinero: dinero,
+};
+
