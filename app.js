f39f169f8da18550fedcdabde05c3f16119a8e9a
Manuel Henriquez <manuelhm1993@gmail.com>
2023-08-23 23:57:50 -0400

    En vanilla javascript se usa el import y export de módulos ES6 en nodejs se usa módulos CommonJS require y module.exports

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-const frutas = ['Plátano', 'Manzana', 'Plátano', 'Pera'];
+// Cuando se trabaja con nodejs no es necesario colocar la extensión .js equivalente a import frutas from "./modulos/frutas.js";
+const frutas = require('./modulos/frutas'); //require('./modulos/frutas.js'); // Opcional
 
 frutas.forEach((fruta) => {
     console.count(fruta);
---
+++ modulos/frutas.js
@@ -0,0 +1,4 @@
+const frutas = ['Plátano', 'Manzana', 'Plátano', 'Pera'];
+
+// Usar el objeto module con la propiedad exports (equivalente a export default funcion;)
+module.exports = frutas;
