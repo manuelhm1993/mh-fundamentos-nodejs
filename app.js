df95bb592e5f41027a54a707e341dd8b1bdc79e8
Manuel Henriquez <manuelhm1993@gmail.com>
2023-08-24 03:06:56 -0400

    Instalación y uso del paquete cowsay para practicar con npm

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
-// Importar a través de destructory
+// Importar a través de destructory (para referenciar que se buscará dentro de nuestras propios directorios se usa ./)
 const { frutas, dinero } = require('./src/frutas');
+
+// Para indicar que se buscará en node_modules no se usa ./
+const cowsay = require("cowsay");
 
 frutas.forEach((fruta) => {
   console.log(fruta);
 })
 
 console.log(dinero);
+
+// Uso de cowsay
+console.log(cowsay.say({
+    text : "Hola MHenriquez",
+    e : "oO",
+    T : "U "
+}));
