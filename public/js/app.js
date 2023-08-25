5e34d2e97c1b713a71e6cec0e600ddcc9896e884
Manuel Henriquez <manuelhm1993@gmail.com>
2023-08-25 02:13:46 -0400

    Los js y css de la carpeta public son archivos estáticos o vanilla, no se pueden usar expresiones como require, en ese caso se usa ES6 import

--- public/js/app.js
+++ public/js/app.js
@@ -1,2 +1,2 @@
-// Importar un módulo completo dentro del script sin empaquetarlo en un objeto
-require('./../vendor/bootstrap-5.3.1-dist/js/bootstrap.bundle.min.js');
+// Importar un módulo completo dentro del script sin empaquetarlo en un objeto se usa ES6 porque es vanilla javascript
+import './../vendor/bootstrap-5.3.1-dist/js/bootstrap.bundle.min.js';
