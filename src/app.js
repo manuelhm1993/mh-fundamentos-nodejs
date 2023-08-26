fa601fa3426241687eb57939cfe6cf053fff1ea4
Manuel Henriquez <manuelhm1993@gmail.com>
2023-08-26 12:28:12 -0400

    Incluir el app.js dentro de la carpeta src

--- src/app.js
+++ src/app.js
@@ -9,19 +9,22 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 // Establecer el directorio de vistas
-app.set('views', path.join(__dirname, 'src/views'));
+app.set('views', path.join(__dirname, 'views'));
 
 // Establecer el motor de plantillas
 app.set('view engine', 'ejs');
 
-// Middlewares registrar la carpeta pública
-app.use(express.static(path.join(__dirname, 'public')));
+console.log(path.join(__dirname.slice(0, -3), 'public'))
+console.log(path.join(__dirname, 'views'))
+
+// Middlewares registrar la carpeta pública || Eliminar src de la ruta
+app.use(express.static(path.join(__dirname.slice(0, -3), 'public')));
 
 // Importar las rutas ya tratadas con el Middleware web
-app.use('/', require('./src/router/RutasWeb'));
+app.use('/', require('./router/RutasWeb'));
 
 // Equivalente a crear grupos, prefijos y nombres en las rutas
-app.use('/mascotas', require('./src/router/Mascotas'));
+app.use('/mascotas', require('./router/Mascotas'));
 
 // Middleware para gestionar errores 404
 app.use((req, res, next) => {
