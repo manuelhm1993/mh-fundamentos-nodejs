ce79e248e78221a38608b89bf8376713b35b481d
Manuel Henriquez <manuelhm1993@gmail.com>
2023-08-26 00:45:06 -0400

    Limpiar código innecesario

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,23 @@
-// Resuelve la ruta absoluta de la salida
 const path = require('path');
 
 module.exports = {
-  // Entrada
-  entry: './src/resources/js/app.js', // Archivos de entrada para ser compilados
-  // Salida
+  entry: './src/resources/js/app.js',
   output: {
-    // Nombre del archivo compilado
     filename: 'app.js',
-    // Ruta de archivo compilado
     path: path.resolve(__dirname, 'public/js'),
   },
-  // Cargadores
   module: {
     rules: [
       {
-        // Matriz para indicar los cargadores
         use: ["style-loader", "css-loader", "sass-loader"],
-        // Expresión regular para indicar el tipo de archivos donde se usarán
         test: /\.(css|sass|scss)$/,
-        // Excluir la carpeta node_modules
         exclude: /node_modules/,
       },
       {
-        // Resolución de imagenes
         type: "asset",
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
       }
     ],
   },
-  // Modo de trabajo
   mode: 'development',
 };
